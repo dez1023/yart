c632907c52012411416a1132e0b59385d2910b29
rfs2iscool <123430228+dez1023@users.noreply.github.com>
2023-02-23 21:05:14 +0000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+
+
+/* */
+
+// title
+
+let logo = ui.img("https://yart.rfs2iscool.repl.co/assets/gui/mclogo.png", sui);
+addToScreen(logo, 0);
+logo.classList.add("logo");
+
+ui.bbtn("Singleplayer", sui, 0, function() { setScreen(1) });
+disable(ui.bbtn("Multiplayer", sui, 0, function() { setScreen(2) }));
+let tsbtns = ui.btns(sui, "Options", 0, function() { setScreen(3) }, "Quit Game", 0, function() { window.close() });
+tsbtns.style.marginTop = "50px"
+
+// worlds
+ui.txt("Select World", sui, 1, 16);
+ui.txt("worlds deez nuts", sui, 1);
+ui.bbtn("Back", sui, 1, function() { setScreen(0) });
+
+// servers
+ui.txt("Play Multiplayer", sui, 2, 16);
+ui.txt("multiplayer deez nuts", sui, 2);
+ui.bbtn("Back", sui, 2, function() { setScreen(0) });
+
+// options
+ui.txt("Options", sui, 3, 16);
+ui.btns(sui, "FOV: Normal", 3, n, "Online...", 3, n)
+ui.bbtn("Back", sui, 3, function() { setScreen(0) });
+
+updateScreens();
---
+++ js/tgui.js
@@ -0,0 +1,103 @@
+let ui = {
+  e: function(el, p) {
+    let e = document.createElement(el);
+    p.appendChild(e);
+    return e;
+  },
+  div: function(p) {
+    let e = ui.e("div", p)
+    return e;
+  },
+  img: function(sc, p) {
+    let e = ui.e("img", p)
+    e.src = sc
+    return e;
+  },
+  a: function(t, p) {
+    let e = ui.e("a", p);
+    e.innerHTML = t;
+    return e;
+  },
+  txt: function(t, p, s, sc) {
+    let e = ui.a(t, p, s);
+    e.classList.add("txt");
+    e.style.fontSize = sc + "px"
+    addToScreen(e, s);
+    return e;
+  },
+  btn: function(t, p, s, f) {
+    let e = ui.e("button", p);
+    e.innerHTML = t;
+    e.onclick = f;
+    addToScreen(e, s);
+    return e;
+  },
+  bbtn: function(t, p, s, f) {
+    let e = ui.btn("", p, s, f);
+    let txt = ui.txt(t, e, s);
+    e.classList.add("bbtn");
+    return e;
+  },
+  btns: function(p, t, s, f, t2, s2, f2) {
+    let cont = ui.div(p)
+    cont.classList.add("btns")
+
+    let e = ui.btn("", cont, s, f);
+    let txt = ui.txt(t, e, s);
+    e.classList.add("sbtn");
+
+    let e2 = ui.btn("", cont, s2, f2);
+    let txt2 = ui.txt(t2, e2, s2);
+    e2.classList.add("sbtn2")
+    return cont;
+  },
+  slider: function(t, p, s, f) {
+    let e = ui.btn("", p, s, f);
+    let txt = ui.txt(t, e, s);
+    e.classList.add("slider");
+    return e;
+  },
+}
+
+let cs = 0
+let db = document.getElementById("ui")
+let sui = ui.div(db)
+sui.classList.add("sui")
+sui.id = "sui"
+
+function updateScreens() {
+  s.forEach(function(sc, i) {
+    if (i == cs) {
+      sc.forEach(function(e) {
+        e.style.display = "block";
+      })
+    } else {
+      sc.forEach(function(e) {
+        e.style.display = "none";
+      })
+    }
+  })
+}
+
+function addToScreen(e, sn) {
+  s[sn].push(e);
+}
+
+function setScreen(sn) {
+  cs = sn;
+  updateScreens();
+}
+
+function disable(e) {
+  e.classList.add("dis");
+  e.onclick = null;
+}
+
+function n() { }
+
+let s = [
+  [], // title
+  [], // worlds
+  [], // multiplayer
+  [], // options
+]
