37f031ffd2e11d2b1576191d4a85a125caea0ac6
rfs2iscool <123430228+dez1023@users.noreply.github.com>
2023-03-03 02:12:11 +0000

    sdijsadkijadslk

--- js/game.js
+++ js/game.js
@@ -1,13 +1,25 @@
 let blocks
 let getBlock
 
-fetch(assets+"blocks.json")
-  .then(res => res.json())
-  .then(json => {
-    blocks = json;
-    getBlock = (blocks,id) => Object.keys(blocks).find(key => blocks[key] === id);
-    return json;
-  });
+/*
+
+fetch(assets+'blocks.json')
+  .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+         console.log(json);
+      });
+
+*/
+
+async function loadBlocks() {
+  const response = await fetch(assets+'blocks.json');
+  const names = await response.json();
+  console.log(names); 
+}
+
+loadBlocks()
 
 function runGame() {
 
@@ -16,9 +28,9 @@ function runGame() {
 function initSP() {
   setScreen(null)
   setScene(gameScene, gameCam, runGame)
-  createBlock(2,0,0,0)
+  createBlock(2, 0, 0, 0)
 }
 
 function createBlock(id, x, y, z) {
-  let b = getBlock(blocks,id)
+  let b = getBlock(blocks, id)
 }
