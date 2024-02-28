b7218905a183bf3e23160cfc3f007a597b2468e6
dez1023 <123430228+dez1023@users.noreply.github.com>
2024-02-28 13:32:26 -0800

    zoit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-const address = "https://yart.rfs2iscool.repl.co/"
+const address = "https://dez1023.github.io/yart/"
 //const address = "https://dez1023.github.io/yart/"
 const assets = address+"assets/"
